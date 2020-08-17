67ebb7f0a49a1e617bc5cbcd048a2e2c327dea49
Phimlaphat Surayaem <fernphimlaphat_44@hotmail.com>
2020-08-17 22:13:19 +0700

    add printf

--- workWeekend_2_1/Source.cpp
+++ workWeekend_2_1/Source.cpp
@@ -1,20 +1,24 @@
 ﻿#include<stdio.h>
+
 int main()
 {
-	int a, c = 0;
-	scanf("%d", &a);
-	for (int i = 1; i <= a; i++)
+	int a ,i, sum = 0;
+	scanf("%ld", &a);
+	printf("Prime number of %d = ",a);
+	for ( i = 1; i <= a; i++)
 	{
 		if (a % i == 0)
 		{
 			if ((i == 1 || i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0) && (i != 2 && i != 3 && i != 5 && i != 7))
 			{
 				continue;
+				sum += i;
 			}
 			printf("%d ", i);
 
 		}
 
-	}
+    }
+	printf("\n");
 
 }
