4ff5d0a4723648b8ebbf122e3a7c1ffd390a02bd
Phimlaphat Surayaem <fernphimlaphat_44@hotmail.com>
2020-08-17 15:25:29 +0700

    Add project files.

---
+++ workWeekend_2_1/Source.cpp
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+int main()
+{
+	int a, c = 0;
+	scanf("%d", &a);
+	for (int i = 1; i <= a; i++)
+	{
+		if (a % i == 0)
+		{
+			if ((i == 1 || i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0) && (i != 2 && i != 3 && i != 5 && i != 7))
+			{
+				continue;
+			}
+			printf("%d ", i);
+
+		}
+
+	}
+
+}
